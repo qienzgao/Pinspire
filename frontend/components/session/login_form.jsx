2806d7ad2a0e85775377cfb3ba48b21b705ea711
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-01 17:22:22 -0500

    middle of login form

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -38,9 +38,18 @@ class LoginForm extends React.Component {
     render() {
         return (
             <form className="login-form-container">
-                Welcome to Pinspire!
-                <br />
-                <button onClick={this.props.close} className="close-button">X</button>
+                <button onClick={this.props.close} className="close-button">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <div className='pinspire-sign'>
+                    <div className='pinspire-img'>
+                        logo
+                    </div>
+                </div>
+                <br/>
+                <div className='form-title'>
+                    <h1>Welcome to Pinspire</h1>
+                </div>
                 {this.renderErrors()}
                 <div className="login-form">
                     <br />
@@ -48,15 +57,24 @@ class LoginForm extends React.Component {
                         value={this.state.username}
                         onChange={this.update('email')}
                         className="login-input"
+                        placeholder='Email'
                     />
                     <br />
                     <input type="password"
                         value={this.state.password}
                         onChange={this.update('password')}
                         className="login-input"
+                        placeholder='Password'
                     />
                     <br />
+                    <div className='forget'>
+                        <label>Forgot your password?</label>
+                    </div>
+                    <br />
                     <button className="login-button" type="submit" onClick={this.handleSubmit}>Log in</button>
+                    <br />
+                    <h3>OR</h3>
+                    <button>Demo log in</button>
                     {this.props.signup}
                 </div>
             </form>

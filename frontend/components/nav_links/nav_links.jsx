2f349a6ab38f5d0f1ef1434c0b01b83c8dbcb32c
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-01 17:22:22 -0500

    middle of login form

--- frontend/components/nav_links/nav_links.jsx
+++ frontend/components/nav_links/nav_links.jsx
@@ -4,6 +4,7 @@ const NavLinks = ({ currentUser, logout, open}) => {
     const sessionLinks = () => (
         <div className='header'>
             <div className='pinspire-logo'>
+                <img className='pinspire-pic' />
                 <h1 className='pinspire-title'>Pinspire</h1>
             </div>
 
@@ -13,12 +14,12 @@ const NavLinks = ({ currentUser, logout, open}) => {
                         <a href="">About</a>
                     </div>
 
-                    <div className="git-button">
-                        <a href="https://github.com/qienzgao">Github</a> 
-                    </div>
-
                     <div className="linkedin-button" >
                         <a href="https://www.linkedin.com/in/qien-gao-1390071a7/">Linkedin</a> 
+                    </div>
+                    
+                    <div className="git-button">
+                        <a href="https://github.com/qienzgao">Github</a> 
                     </div>
                 </div>
 

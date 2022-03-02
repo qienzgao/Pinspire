8cc8113168cfe7c1e2afc5e015433d6ee25d3b85
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-02 01:30:19 -0500

    splash pics

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Splash = () => {
+    return (
+        <div>
+            <div className="title">
+                <h2 className="stable">Get your next</h2>
+                <div className="categories">
+                    <div className="changing"><h2 >Category one</h2></div>
+                    <div className="changing"><h2 >Category two</h2></div>
+                    <div className="changing"><h2 >Category three</h2></div>
+                    <div className="changing"><h2 >Category four</h2></div>
+                </div>
+            </div>
+            <div className='background-pics'>
+                <div className="grid">
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                    <div className="column">
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                        <div className="pic"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ); 
+}; 
+
+export default Splash; 

84fc98cd41e572442029af24fb65f0d83ccad23c
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-02 01:30:19 -0500

    splash pics

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavLinksContainer from "./nav_links/nav_links_container";
 import AllModals from "./modal/modal_container"; 
+import Splash from "./splash/splash";
 // import NavLinks from "./nav_links/nav_links";
 // import LoginFormContainer from "./session/login_form_container";
 // import SignUpFormContainer from "./session/signup_form_container";
@@ -12,6 +13,7 @@ const App = () => (
         <header>
             <NavLinksContainer />
         </header>
+        <Splash/>
     </div>
 );
 

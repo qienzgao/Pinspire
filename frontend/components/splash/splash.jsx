268b2d832e3cf122e537a217d198548e7c6460be
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-02 03:39:17 -0500

    start splash animation

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,16 +7,16 @@ const Splash = () => {
                 <h2 className="stable">Get your next</h2>
                 <div className="categories">
                     <div className="changing">
-                        <h2 id="dinner">weeknight dinner idea</h2>
+                        <h2 id="dinner" className="fade">weeknight dinner idea</h2>
                     </div>
                     <div className="changing">
-                        <h2 id="decor">home decor idea</h2>
+                        <h2 id="decor" className="fade">home decor idea</h2>
                     </div>
                     <div className="changing">
-                        <h2 id="outfit">new look outfit</h2>
+                        <h2 id="outfit" className="fade">new look outfit</h2>
                     </div>
                     <div className="changing">
-                        <h2 id="green">green thumb idea</h2>
+                        <h2 id="green" className="fade">green thumb idea</h2>
                     </div>
                 </div>
             </section>
@@ -25,49 +25,49 @@ const Splash = () => {
             </section>
             <div className='background-pics'>
                 <div className="grid">
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>
                     </div>
-                    <div className="column">
+                    <div className="column fade">
                         <div className="pic"></div>
                         <div className="pic"></div>
                         <div className="pic"></div>

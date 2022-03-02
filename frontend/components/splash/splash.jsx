7cf7545dcd0af5bc8a03fb7e4d4b642a2342a358
Qien Gao <qiengao@Qiens-MacBook-Pro.local>
2022-03-02 03:26:23 -0500

    still page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,15 +3,26 @@ import React from "react";
 const Splash = () => {
     return (
         <div>
-            <div className="title">
+            <section className="main-text">
                 <h2 className="stable">Get your next</h2>
                 <div className="categories">
-                    <div className="changing"><h2 >Category one</h2></div>
-                    <div className="changing"><h2 >Category two</h2></div>
-                    <div className="changing"><h2 >Category three</h2></div>
-                    <div className="changing"><h2 >Category four</h2></div>
+                    <div className="changing">
+                        <h2 id="dinner">weeknight dinner idea</h2>
+                    </div>
+                    <div className="changing">
+                        <h2 id="decor">home decor idea</h2>
+                    </div>
+                    <div className="changing">
+                        <h2 id="outfit">new look outfit</h2>
+                    </div>
+                    <div className="changing">
+                        <h2 id="green">green thumb idea</h2>
+                    </div>
                 </div>
-            </div>
+            </section>
+            <section className="second-text">
+                <h2 className="signup-text">Sign up to get your ideas</h2>
+            </section>
             <div className='background-pics'>
                 <div className="grid">
                     <div className="column">

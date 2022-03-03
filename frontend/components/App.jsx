998cbb6f0f1f6f95fe77ba557fe6c6ec6141a77e
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-03 16:40:52 -0500

    app

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import NavLinksContainer from "./nav_links/nav_links_container";
 import AllModals from "./modal/modal_container"; 
 import Splash from "./splash/splash";
-// import AuthRoute from '../util/route_util'; 
-// import NavLinks from "./nav_links/nav_links";
-// import LoginFormContainer from "./session/login_form_container";
-// import SignUpFormContainer from "./session/signup_form_container";
 
 
 const App = () => (

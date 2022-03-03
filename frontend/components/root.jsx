eb48f2b8c882a790d2522e5ae72c6b7d3aa12757
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-03 16:41:59 -0500

    application

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from 'react-router-dom';
-import App from './app.jsx'; 
+import App from './application.jsx'; 
 
 const Root = ({ store }) => (
     <Provider store={store}>

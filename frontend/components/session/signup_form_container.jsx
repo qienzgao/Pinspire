8cc1cffeedb4fd6de9eb9ee3c7a2625023c4068e
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-03 14:46:12 -0500

    jbuilder

--- frontend/components/session/signup_form_container.jsx
+++ frontend/components/session/signup_form_container.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import SignupForm from './signup_form';
 import { connect } from 'react-redux';
-import { signup, removeErrors } from '../../actions/session_actions';
+import { signup, login, removeErrors } from '../../actions/session_actions';
 import { open, close } from '../../actions/modal_actions';
 
 const mSTP = ( state ) => ({
-    errors: state.errors.SessionErrorsReducer,
+    errors: state.errors.session, 
 });
 
 const mDTP = dispatch => ({
     submit: (user) => dispatch(signup(user)),
+    loginDemo: (user) => dispatch(login(user)), 
     login: (
         <button className="otherwise" onClick={() => dispatch(open('login'))}>
             Already a member? Log in

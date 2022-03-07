45db8b1ca9bad9a1985e8da906627e1778f33c65
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-07 14:49:15 -0500

    active storage installed

--- frontend/components/pin/pin_create_container.jsx
+++ frontend/components/pin/pin_create_container.jsx
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
 import { createPin } from '../../actions/pin_actions';
 import PinCreate from './pin_create';
+import { open } from '../../actions/modal_actions';
 
 const mSTP = state => ({
-    user: state.entities.users[state.session.currentUser],
+    user: state.entities.users[state.session.id],
 })
 
 const mDTP = dispatch => ({
-    submit: (pin) => dispatch(createPin(pin))
+    submit: (pin) => dispatch(createPin(pin)),
+    saved: () => dispatch(open('saved')),
 })
 
 export default connect(mSTP, mDTP)(PinCreate);

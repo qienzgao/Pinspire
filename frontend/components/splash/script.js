d5da70b0a8b160f1b64d24e9cc0cb9c62158c2b5
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-10 04:02:14 -0500

    test

---
+++ frontend/components/splash/script.js
@@ -0,0 +1,58 @@
+const grids = document.querySelectorAll('.grid');
+const headings = document.querySelectorAll('.main-text .categories .text')
+
+const enterScreen = index => {
+    const grid = grids[index]
+    const heading = headings[index]
+    const gridColumns = grid.querySelectorAll('.column')
+
+    grid.classList.add('active')
+
+    gridColumns.forEach(element => {
+        element.classList.remove('before', 'after')
+    })
+
+    heading.classList.remove('before', 'after')
+}
+
+const exitScreen = (index, exitDelay) => {
+    const grid = grids[index]
+    const heading = headings[index]
+    const gridColumns = grid.querySelectorAll('.column')
+
+    gridColumns.forEach(element => {
+        element.classList.add('after')
+    })
+
+    heading.classList.add('after')
+
+    setTimeout(() => {
+        grid.classList.remove('active')
+    }, exitDelay)
+}
+
+const setupAnimationCycle = ({ timePerScreen, exitDelay }) => {
+    const cycleTime = timePerScreen + exitDelay
+    let nextIndex = 0
+
+    function nextCycle() {
+        const currentIndex = nextIndex
+
+        enterScreen(currentIndex)
+
+        setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen)
+
+        nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1
+    }
+
+    nextCycle()
+
+    setInterval(nextCycle, cycleTime)
+}
+
+setupAnimationCycle({
+    timePerScreen: 2000,
+    exitDelay: 200 * 7
+})
+
+

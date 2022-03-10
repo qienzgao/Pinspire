9031f4241547abf1ccf5594a355822c62fd2b252
Qien Gao <qiengao@Qiens-MBP.lan>
2022-03-10 04:02:14 -0500

    test

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PinIndexItem from "../pin/pin_index_item";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";  
+import { useEffect } from 'react';
 
 class Splash extends React.Component {
     constructor(props){
@@ -9,6 +11,10 @@ class Splash extends React.Component {
 
     componentDidMount() {
         this.props.fetchPins();
+        const script = document.createElement("script");
+        script.src = 'script.js';
+        script.async = true;
+        document.body.appendChild(script);
     }
     
     render(){
@@ -16,22 +22,25 @@ class Splash extends React.Component {
         const pinsArray = Object.values(pins)
         const sizes = ["ex-small", "small", "medium", "large", "ex-large"]
 
-        const mainPage = () => (
-            <div>
+        const mainPage = () => {
+            return (
+            <div id="mainpage" ref={el => (this.instance = el)}>
+                
                 <section className="main-text">
                     <h2 className="stable">Get your next</h2>
+
                     <div className="categories">
                         <div className="changing">
-                            <h2 id="dinner" className="fade">weeknight dinner idea</h2>
+                            <h2 className="text before">weeknight dinner idea</h2>
                         </div>
                         <div className="changing">
-                            <h2 id="decor" className="fade">home decor idea</h2>
+                            <h2 className="text before">home decor idea</h2>
                         </div> 
                         <div className="changing">
-                            <h2 id="outfit" className="fade">new look outfit</h2>
+                            <h2 className="text before">new look outfit</h2>
                         </div>
                         <div className="changing">
-                            <h2 id="green" className="fade">green thumb idea</h2>
+                            <h2 className="text before">green thumb idea</h2>
                         </div>
                     </div>
                 </section>
@@ -40,59 +49,216 @@ class Splash extends React.Component {
                 </section>
                 <div className='background-pics'>
                     <div className="grid">
+
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                         <div className="column">
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
-                            <div className="pic"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                    </div>
+
+                    <div className="grid">
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                    </div>
+
+                    <div className="grid">
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                    </div>
+
+                    <div className="grid">
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                        </div>
+                        <div className="column">
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
+                            <div className="pic before"></div>
                         </div>
                     </div>
                 </div>
             </div>
-        ); 
+        )}; 
 
 
         const homePage = () => (

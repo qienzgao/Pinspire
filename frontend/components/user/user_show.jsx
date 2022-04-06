cbc1383bbef51a07c3afc776c2262b91d9971647
Qien Gao <qienzgao@gmail.com>
2022-04-06 13:49:59 -0400

    fix some styling

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -161,7 +161,7 @@ class UserShow extends React.Component {
                             </div>
                         </div> : null}
                     {this.state.saved ? <div>
-                        <h1>hello</h1>
+                        <h1>Not Yet Done</h1>
                     </div> : null}
                     
                 </section>

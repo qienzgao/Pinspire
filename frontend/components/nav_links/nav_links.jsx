eec8fa379214279bf557d2e568e55e12f5d1bf2d
Qien Gao <qienzgao@gmail.com>
2022-05-11 22:38:50 -0400

    start search bar

--- frontend/components/nav_links/nav_links.jsx
+++ frontend/components/nav_links/nav_links.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 const NavLinks = ({ currentUser, logout, open}) => {
+
     const sessionLinks = () => (
         <div className='header'>
             <div className='pinspire-logo'>
@@ -56,7 +57,10 @@ const NavLinks = ({ currentUser, logout, open}) => {
                     </div>
                 </div>
                     <div className='search-bar-container'>
-                        <input className="search-bar" type="text" placeholder='Search'/>
+                        <input className="search-bar"
+                            type="text" 
+                            placeholder='Search'
+                            />
                     </div>
 
                 <div className="personal">
